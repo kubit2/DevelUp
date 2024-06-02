52174f8a660291f839aac789f017f12759d2eb03
희은 <datakubit2@gmail.com>
2024-06-02 17:55:35 +0900

    chore: 개발 환경 세팅

---
+++ src/main/java/com/example/develup/DevelUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.develup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/develup/DevelUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.develup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
